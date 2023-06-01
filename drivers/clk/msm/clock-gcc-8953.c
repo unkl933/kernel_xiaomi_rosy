89fb021c9c080efef696679911b9ee71de579e4c
dopamine <crack1933@gmail.com>
2023-06-02 00:18:54 +0700

    Rosy: OC GPU & CPU

--- drivers/clk/msm/clock-gcc-8953.c
+++ drivers/clk/msm/clock-gcc-8953.c
@@ -387,6 +387,8 @@ static struct clk_freq_tbl ftbl_gfx3d_clk_src[] = {
 	F_MM( 510000000,    1020000000,               gpll3,    1,    0,     0),
 	F_MM( 560000000,    1120000000,               gpll3,    1,    0,     0),
 	F_MM( 650000000,    1300000000,               gpll3,    1,    0,     0),
+	F_MM( 700000000,    1400000000,               gpll3,    1,    0,     0),
+	F_MM( 747000000,    1494000000,               gpll3,    1,    0,     0),
 
 	F_END
 };
@@ -406,7 +408,7 @@ static struct clk_freq_tbl ftbl_gfx3d_clk_src_sdm450[] = {
 	F_MM( 460800000, FIXED_CLK_SRC,       gpll4_out_aux,  2.5,    0,     0),
 	F_MM( 510000000,    1020000000,               gpll3,    1,    0,     0),
 	F_MM( 560000000,    1120000000,               gpll3,    1,    0,     0),
-	F_MM( 600000000,    1200000000,               gpll3,    1,    0,     0),
+	F_MM( 747000000,    1480000000,               gpll3,    1,    0,     0),
 	F_END
 };
 
